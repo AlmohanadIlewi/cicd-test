f0fd5f4b0414b6a3a8d4dfbb36522cf7a5249c7b
Mohanad Ilewi <138300746+AlmohanadIlewi@users.noreply.github.com>
2023-11-22 11:24:58 +0100

    ci(.github\workflows): added s3 deploy and policy1

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -20,6 +20,29 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:*",
+        Resource  = [
+          "arn:aws:s3:::my-best-website-bucket-abc1234567gkl/*",
+          "arn:aws:s3:::my-best-website-bucket-abc1234567gkl",
+        ]
+      },
+    ]
+  })
+}
+
+
+
 # resource "aws_s3_bucket_public_access_block" "this" {
 #   bucket = aws_s3_bucket.this.id
 
@@ -29,21 +52,21 @@ resource "aws_s3_bucket_website_configuration" "this" {
 #   restrict_public_buckets = false
 # }
 
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource  = [
-          "${aws_s3_bucket.this.arn}/*",
-          "${aws_s3_bucket.this.arn}",
-        ]
-      },
-    ]
-  })
-}
+# resource "aws_s3_bucket_policy" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:*"
+#         Resource  = [
+#           "${aws_s3_bucket.this.arn}/*",
+#           "${aws_s3_bucket.this.arn}",
+#         ]
+#       },
+#     ]
+#   })
+# }
