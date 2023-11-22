ccb550f144a0288546ce0b4e223248802c0e7a51
Mohanad Ilewi <138300746+AlmohanadIlewi@users.noreply.github.com>
2023-11-22 10:06:21 +0100

    ci(.github\workflows): edited s3 bucket name

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,4 @@
 module "s3_website" {
     source      = "./modules/s3-website"
-    bucket_name = "my-best-website-bucket-abc1234567"
+    bucket_name = "my-best-website-bucket-abc1234567cdf"
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,5 +10,4 @@ variable "aws_profile" {
 
 variable "bucket_name" {
     type    = string
-    default = "My lovely bucket for my lovely website"
 }
